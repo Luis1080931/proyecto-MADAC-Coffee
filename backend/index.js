e72790aa6766224ddd21ca988d4bebc701d477f3
Luis1080931 <luisu0616@gmail.com>
2024-02-08 00:34:19 -0500

    change segundo merge

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,23 @@
 import express  from "express";
 import body_parser from 'body-parser'
-<<<<<<< HEAD
 import routeResultados from "./src/routes/route.resultados.js";
 import routeAnalisis from "./src/routes/analisis.routes.js";
 import routeFincas from "./src/routes/fincas.routes.js";
 import routeLotes from "./src/routes/lotes.routes.js";
 import routeVariedades from "./src/routes/variedades.routes.js";
-=======
->>>>>>> devlaup
 
 const servidor = express()
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
 
-<<<<<<< HEAD
 servidor.use('/resultados', routeResultados)
 servidor.use('/analisis', routeAnalisis)
 servidor.use('/fincas', routeFincas)
 servidor.use('/lotes', routeLotes)
 servidor.use('/variedades', routeVariedades)
 
-=======
->>>>>>> devlaup
+
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 })
