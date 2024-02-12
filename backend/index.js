55b7fb7dc11ed3e657c9d408df718dc9aa8418da
Luis1080931 <luisu0616@gmail.com>
2024-02-11 21:17:24 -0500

    arreglos

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,10 @@
 import express  from "express"
 import body_parser from "body-parser"
-import RouteMuestras from "./src/routes/routeMuestras.js";
-import routeVariables from "./src/routes/routeVariables.js";
 
 const servidor = express()
 
 servidor.use(express.json());
 servidor.use(express.urlencoded({ extended : false }));
-
-
-servidor.use("/variables", routeVariables)
-servidor.use("/muestras", RouteMuestras)
-
-//ejs 
-servidor.set('view engine', 'ejs')
-servidor.set('views', './views');
-
-servidor.get('/documents', (req,res)=>{
-    res.render('documents.ejs');
-})
-
-//public styles css
-servidor.use(express.static('./public'));
 
 //listen 
 servidor.listen(3000, () => {
