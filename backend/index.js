a75c90e7bd8d6db36f2674cc7bcc6b703fb05439
alejandroDpAL <alejandropasaje799@gmail.com>
2024-02-13 00:05:55 -0500

    changes index

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,12 @@
 import express  from "express"
 import body_parser from "body-parser"
-import RouteMuestras from "./src/routes/routeMuestras.js";
-import routeVariables from "./src/routes/routeVariables.js";
+
 
 const servidor = express()
 
 servidor.use(express.json());
 servidor.use(express.urlencoded({ extended : false }));
 
-
-servidor.use("/variables", routeVariables)
-servidor.use("/muestras", RouteMuestras)
-
-//ejs 
-servidor.set('view engine', 'ejs')
-servidor.set('views', './views');
-
-servidor.get('/documentvariable', (req,res)=>{
-    res.render('documentVariable.ejs');
-})
-servidor.get('/documentmuestra', (req,res)=>{
-    res.render('documentMuestra.ejs');
-})
-
-//public styles css
-servidor.use(express.static('./public'));
 
 //listen 
 servidor.listen(3000, () => {
