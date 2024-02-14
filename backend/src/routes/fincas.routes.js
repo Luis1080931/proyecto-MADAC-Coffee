22a580ab9a28fa6261bab2adc4e0fd673d18dc01
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-02-14 00:15:53 -0500

    arreglar las rutas

--- backend/src/routes/fincas.routes.js
+++ backend/src/routes/fincas.routes.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import {getFincas,getFinca,postFincas,activar_desactivar_Fincas,actualizarFincas} from '..//controllers/fincas.controller.js';
 
-const router = Router()
-router.get("/finca",getFincas)
-router.get("/finca/:codigo",getFinca)
-router.post("/finca",postFincas)
-router.put("/finca/:codigo",activar_desactivar_Fincas)
-router.patch("/finca/:codigo",actualizarFincas)
-
-export default router;
+const routerFincas = Router()
+routerFincas.get("/listar",getFincas)
+routerFincas.get("/buscar/:codigo",getFinca)
+routerFincas.post("/registrar",postFincas)
+routerFincas.put("/descativar/:codigo",activar_desactivar_Fincas)
+routerFincas.put("/actualizar/:codigo",actualizarFincas)
+export default routerFincas;
--- backend/src/routes/lotes.routes.js
+++ backend/src/routes/lotes.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import {getLotes,getLote,postLotes,activar_desactivar_Lotes,actualizarLotes} from '..//controllers/lotes.controller.js'
 
-const router = Router()
-router.get("/lotes",getLotes)
-router.get("/lotes/:codigo",getLote)
-router.post("/lotes",postLotes)
-router.put("/lotes/:codigo",activar_desactivar_Lotes)
-router.patch('/lotes/:codigo',actualizarLotes)
-export default router;
+const routerLotes = Router()
+routerLotes.get("/listar",getLotes)
+routerLotes.get("/buscar/:codigo",getLote)
+routerLotes.post("/registrar",postLotes)
+routerLotes.put("/desactivar/:codigo",activar_desactivar_Lotes)
+routerLotes.put('/actualizar/:codigo',actualizarLotes)
+export default routerLotes;
