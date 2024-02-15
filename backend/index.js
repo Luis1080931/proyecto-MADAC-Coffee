9ced1345780beb85efeefb17eae967aaa9a76257
david180120003 <bravocarmen264@gmail.com>
2024-02-14 21:56:06 -0500

    documentUsuarios

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express  from "express";
 import body_parser from 'body-parser'
 
+
 const servidor = express()
+
+
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
