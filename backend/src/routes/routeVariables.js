5815a72f15f5a0362319ac1e64adf2486c0ef3c6
alejandroDpAL <alejandropasaje799@gmail.com>
2024-02-27 12:03:24 -0500

    validation

--- backend/src/routes/routeVariables.js
+++ backend/src/routes/routeVariables.js
@@ -1,18 +1,12 @@
 import { Router } from "express";
 import { listarVariables, CrearVariable, ActualizarVariable, desactivarVariable, buscarvariable } from "../controllers/controllerVariables.js";
-import { check } from "express-validator";
+import { validacionVariable } from "../validate/variable.js";
 
 const routeVariables = Router()
 
 routeVariables.get("/listarvariable", listarVariables)
 
-routeVariables.post("/crearvariable",
-    [
-        check('nombre', 'el nombre de la variable es requerido').not().isEmpty().isLength({max:200}),
-        check('fk_tipo_analisis', 'la fk tipo analisis es requerida ').not().isEmpty().isNumeric(),
-        check('estado', 'escribe un estado').not().isEmpty().isIn('activo', 'inactico')
-    ],
- CrearVariable)
+routeVariables.post("/crearvariable",validacionVariable,CrearVariable)
 
 routeVariables.put("/actualizarvariable/:codigo", ActualizarVariable)
 routeVariables.put("/desactivarVariable/:codigo", desactivarVariable)
