baea0bcd49033377fc8171d9d0c283c6799e1ee4
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-02-28 00:08:26 -0500

    indexbasico

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,10 @@
 import express  from "express";
 import body_parser from 'body-parser'
-import lotes from './src/routes/lotes.routes.js'
-import fincas from './src/routes/fincas.routes.js'
 
 const servidor = express()
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
-servidor.use("/lotes",lotes)
-servidor.use("/fincas",fincas)
 
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
