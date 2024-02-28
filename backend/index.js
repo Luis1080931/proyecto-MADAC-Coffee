6c156bd360db01c18dab2228923d3d6dcc14a8b8
david180120003 <bravocarmen264@gmail.com>
2024-02-28 09:09:36 -0500

    actualizacion

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,14 @@ import express  from "express";
 import body_parser from 'body-parser'
 
 
+
 const servidor = express()
 
 
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
+
 
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
