d79366af9a7a11757821deeab50cf256874fc1cb
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-02-28 11:58:11 -0500

    validacion en carpeta

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 import express  from "express";
 import body_parser from 'body-parser'
+import fincas from './src/routes/fincas.routes.js'
+import lotes from './src/routes/lotes.routes.js'
 
 const servidor = express()
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
-
+servidor.use("/fincas",fincas)
+servidor.use("/lotes",lotes)
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 })
--- backend/src/routes/lotes.routes.js
+++ backend/src/routes/lotes.routes.js
@@ -1,18 +1,12 @@
 import { Router } from "express";
 import {getLotes,getLote,postLotes,desactivar_Lotes,actualizarLotes} from '..//controllers/lotes.controller.js'
-import { check } from "express-validator";
+import { validarLotes } from "../../validate/lotes.validate.js";
+import { validarActLotes } from "../../validate/lotes.validate.js";
 
 const routerLotes = Router()
 routerLotes.get("/listar",getLotes)
 routerLotes.get("/buscar/:codigo",getLote)
-routerLotes.post("/registrar",[check('numero_arboles','no dijitaste ningun dato valido en el campo de numero de arboles').not().isEmpty().isNumeric(),
-                               check('fk_finca','no dijitaste ningun dato valido en el campo de finca').not().isEmpty().isNumeric(),
-                               check('fk_variedad','no dijitaste ningun dato valido en el campo de variedad').not().isEmpty().isNumeric(),
-                               check('estado','no dijitaste ningun dato valido en el campo de estado').not().isEmpty().isNumeric()
-,],postLotes)
+routerLotes.post("/registrar",validarLotes,postLotes)
 routerLotes.put("/desactivar/:codigo",desactivar_Lotes)
-routerLotes.put('/actualizar/:codigo',[check('numero_arboles','no dijitaste ningun dato valido en el campo de numero de arboles').not().isEmpty().isNumeric(),
-                                         check('fk_finca','no dijitaste ningun dato valido en el campo de finca').not().isEmpty().isNumeric(),
-                                         check('fk_variedad','no dijitaste ningun dato valido en el campo de variedad').not().isEmpty().isNumeric()
-,],actualizarLotes)
+routerLotes.put('/actualizar/:codigo',validarActLotes,actualizarLotes)
 export default routerLotes;
