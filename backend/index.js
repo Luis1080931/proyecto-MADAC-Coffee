201e8fc8a87d8d2f36e5a292c938d3b306461183
david18012003 <bravocarmne264@gmail.com>
2024-03-03 16:53:18 -0500

    seguridad

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express  from "express";
 import body_parser from 'body-parser'
 import rutas from './src/routes/usuarios.route.js'
+import rutaValidacion from "./src/routes/seguridad.route.js";
 
 
 const servidor = express()
@@ -10,7 +11,6 @@ const servidor = express()
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
 
-servidor.use('/usuarios', rutas)
 
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
--- backend/src/routes/usuarios.route.js
+++ backend/src/routes/usuarios.route.js
@@ -1,19 +1,20 @@
 import { Router } from "express";
 import { desactivarUsuarios, actualizarUsuarios, registrarUsuarios, listarUsuarios, buscarUsuarios } from "./../controllers/usuarios.controller.js";
 import { validarUsuarios } from "../../validate/usuarios.validate.js";
+import { validarToken } from "../controllers/seguridad.controller.js";
  
 
 const routeUsuarios = Router()
 
 routeUsuarios.post("/registrar", validarUsuarios, registrarUsuarios)
 
-routeUsuarios.put("/actualizar/:identificacion", validarUsuarios, actualizarUsuarios)
+routeUsuarios.put("/actualizar/:identificacion", validarToken,validarUsuarios, actualizarUsuarios)
 
-routeUsuarios.put("/desactivar/:identificacion", desactivarUsuarios)
+routeUsuarios.put("/desactivar/:identificacion", validarToken,desactivarUsuarios)
 
-routeUsuarios.get("/listar", listarUsuarios)
+routeUsuarios.get("/listar", validarToken ,listarUsuarios)
 
-routeUsuarios.get("/buscar/:identificacion",buscarUsuarios)
+routeUsuarios.get("/buscar/:identificacion",validarToken,buscarUsuarios)
 
 
 export default routeUsuarios
