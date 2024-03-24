fa8b2c63cc0e45eddce277b82eebc4a6d7219161
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-03-24 18:08:53 -0500

    listar nombre caficultor,listar nombre variedad

--- backend/validate/fincas.validate.js
+++ backend/validate/fincas.validate.js
@@ -4,7 +4,7 @@ import { check } from "express-validator";
 export const validarFincas=[
 check('dimension_mt2', 'no dijitaste ningun dato valido en el campo de dimension').not().isEmpty().isFloat(),
 check('fk_caficultor','no dijitaste ningun dato valido en el campo de caficultor').not().isEmpty().isNumeric(),
-check('municipio','no dijitaste ningun dato valido en el campo de municipio').not().isEmpty().isString(),
+check('municipio','no dijitaste ningun dato valido en el campo de municipio').not().isEmpty().isNumeric(),
 check('vereda','no dijitaste ningun dato valido en el campo de vereda').not().isEmpty().isString(),
 check('estado','no dijitaste ningun dato valido en el campo de estado').not().isEmpty().isNumeric()
 ]
