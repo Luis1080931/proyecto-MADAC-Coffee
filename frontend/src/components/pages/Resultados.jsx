106ec0fc774f531cd6d846fedae8edbad9da216c
Luis1080931 <luisu0616@gmail.com>
2024-03-31 22:37:41 -0500

    axios para traer datos y listarlos en data-table

--- frontend/src/components/pages/Resultados.jsx
+++ frontend/src/components/pages/Resultados.jsx
@@ -1,16 +1,115 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import DataTable from 'react-data-table-component'
 import { Header } from './../molecules/Header.jsx'
 import { Link } from 'react-router-dom';
 import { Buscador } from '../atoms/Buscador.jsx';
 import { ButtonRegister } from '../atoms/ButtonRegister.jsx';
 import { ButtonActualizar } from '../atoms/ButtonActualizar.jsx';   
-import ResultadosModal from '../organisms/ResultadosModal.jsx';
+import ResultadosModal from '../organisms/ResultadosModal.jsx'
+import axios from 'axios';
+
 
 export function Resultados () {
 
+    const baseURL = 'http://localhost:3000/resultados/listar'
+   /*  const token = localStorage.getItem('token') */
 
-    const colums = [
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        try {
+            axios.get(baseURL).then((response) => {
+                console.log(response)
+                setData(response.data)
+            })
+        } catch (error) {
+            console.log('Error de servidor' + error)
+        }
+    }, [])
+
+    const columns = [
+        {
+            name:  'Id',
+            selector: 'codigo',
+            sortable: true
+        },
+        {
+            name: 'Fecha',
+    /*         selector: 'fecha', */
+            sortable: true
+        },
+        {
+            name: 'Análisis',
+          /*   selector: 'fk_analisis', */
+            sortable: true
+        },
+        {
+            name: 'Variable',
+           /*  selector: 'fk_variables', */
+            sortable: true
+        },
+        {
+            name: 'Observaciones',
+            /* selector: 'observaciones', */
+            sortable: true
+        },
+        {
+            name: 'Valor',
+    /*         selector: 'valor', */
+            sortable: true
+        },
+        {
+            name: 'Estado',
+     /*        selector: 'estado', */
+            sortable: true
+        }
+    ]
+
+    const paginaOpciones={
+        rowsPerPageText: 'Filas por página',
+        rangeSeparatorText: 'de',
+        selectAllRowsItem: true,
+        selectAllRowsText: 'Todos'
+    }
+    
+    function handleFilter (event){
+        const newData = data.filter(row => {
+            return row.variable.toLowerCase().includes(event.target.value.toLowerCase())
+        })
+        setRecords(newData)
+    }
+
+    const [modalOpen, setModalOpen] = useState(false)
+  return (
+    
+    <div>
+        <Header title="Resultados" />
+        <div className='w-full flex flex-col justify-center items-center p-10'>
+            
+            <Buscador handler={handleFilter} />
+            <ButtonRegister click={() => setModalOpen(true)} />
+            <ResultadosModal open={modalOpen} onClose={() => setModalOpen(false)} />
+
+            <DataTable
+                columns={columns}
+                data={data}
+                title="Resultados registrados"
+                fixedHeader 
+                pagination
+                paginationComponentOptions={paginaOpciones}
+            >
+
+            </DataTable>
+            
+        </div>
+    </div>
+  )
+}
+
+
+    /* const [records, setRecords] = useState(data) */
+
+/* const colums = [
         {
             name: 'Código',
             selector: row => row.codigo,
@@ -112,47 +211,5 @@ export function Resultados () {
             estado: "inactivo", 
             acciones: <div><button className='bg-[#FFC700] p-2 rounded-lg text-sm font-bold' type="button"><Link to={`/resultadosactualizar`}>Actualizar</Link></button> <button className='bg-[#ED6158] p-2 rounded-lg text-sm font-bold' type="button">Desactivar</button></div> 
         }
-    ]
+    ] */
 
-    const paginaOpciones={
-        rowsPerPageText: 'Filas por página',
-        rangeSeparatorText: 'de',
-        selectAllRowsItem: true,
-        selectAllRowsText: 'Todos'
-    }
-
-    const [records, setRecords] = useState(data)
-    
-    function handleFilter (event){
-        const newData = data.filter(row => {
-            return row.variable.toLowerCase().includes(event.target.value.toLowerCase())
-        })
-        setRecords(newData)
-    }
-
-    const [modalOpen, setModalOpen] = useState(false)
-  return (
-    
-    <div>
-        <Header title="Resultados" />
-        <div className='w-full flex flex-col justify-center items-center p-10'>
-            
-            <Buscador handler={handleFilter} />
-            <ButtonRegister click={() => setModalOpen(true)} />
-            <ResultadosModal open={modalOpen} onClose={() => setModalOpen(false)} />
-
-            <DataTable
-                columns={colums}
-                data={records}
-                title="Resultados registrados"
-                fixedHeader 
-                pagination
-                paginationComponentOptions={paginaOpciones}
-            >
-
-            </DataTable>
-            
-        </div>
-    </div>
-  )
-}
