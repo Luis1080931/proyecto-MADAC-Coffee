4941e020c8e8c7ef8d72dcd14f276b7252053ae9
Svargasowz <sergio.vargasramirez.es@gmail.com>
2024-04-03 22:25:51 -0500

    formularioFinca

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,12 @@ import { ActualizarFincas } from "./components/pages/ActualizarFincass.jsx"
 import { Lotes } from "./components/pages/Lotes.jsx"
 import { RegistrarLotes } from "./components/pages/RegistrarLotes.jsx"
 import { ActualizarLotes } from "./components/pages/ActualizarLotes.jsx"
+import {FormFincass} from './components/molecules/FormFincass.jsx'
 function App() {
 
   return (
     <BrowserRouter>    
-     { <ActualizarLotes/>}
+    { <FormFincass/>}
     </BrowserRouter>
 
   )
