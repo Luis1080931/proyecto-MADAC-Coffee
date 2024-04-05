bc4231c3ab11aeadeb7b7d240896b2a32c3c73d6
Luis1080931 <luisu0616@gmail.com>
2024-04-04 23:51:23 -0500

    solucionando errores del dashboard

--- frontend/src/components/pages/Dashboard.jsx
+++ frontend/src/components/pages/Dashboard.jsx
@@ -43,8 +43,6 @@ export const Dashboard = () => {
 
             <SideBarUser />
 
-            <FaAlignJustify size={30} className="flex mr-3 cursor-pointer"/>
-
         </div>
         <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
           <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='w-full h-full bg-center bg-cover duration-500'>
