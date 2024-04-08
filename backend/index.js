5cafaac431c373d1c0cdb8e0f8cf78d9b2487dc3
david18012003 <bravocarmen264@gmail.com>
2024-04-07 23:00:43 -0500

    arreglo de la tabla de listar usuarios implementando la libreria material ui y arreglo de los formularios

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,20 @@
 import express  from "express";
 import body_parser from 'body-parser'
-import rutas from './src/routes/usuarios.route.js'
-import rutaValidacion from "./src/routes/seguridad.route.js";
+// import rutas from './src/routes/usuarios.route.js'
+// import rutaValidacion from "./src/routes/seguridad.route.js";
+// import cors from 'cors'
+// import routeUsuarios from "./src/routes/usuarios.route.js";
 
 
 const servidor = express()
-
+// servidor.use(cors())
 
 
 servidor.use(body_parser.json())
 servidor.use(body_parser.urlencoded({extend: false}))
+
+// servidor.use('/usuarios', routeUsuarios)
+// servidor.use(rutaValidacion)
 
 
 servidor.listen(3000, () => {
