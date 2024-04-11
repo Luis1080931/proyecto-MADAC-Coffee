2d5689d74e5bcd4f13de00de686ad437938a38a3
Luis1080931 <luisu0616@gmail.com>
2024-04-11 00:26:40 -0500

    solucionando problema de actualizacion en el modulo usuarios

--- backend/src/controllers/usuarios.controller.js
+++ backend/src/controllers/usuarios.controller.js
@@ -95,7 +95,7 @@ export const actualizarUsuarios = async (req,res)=>{
 
 
         const { identificacion } = req.params
-        const{ telefono, nombre, correo_electronico, tipo_usuario, estado} = req.body
+        const{ telefono, nombre, correo_electronico, tipo_usuario } = req.body
         
         const [ usuarioAnterior ] = await pool.query("select * from usuarios where identificacion=?", [identificacion])
 
@@ -104,7 +104,7 @@ export const actualizarUsuarios = async (req,res)=>{
         nombre='${nombre ? nombre : usuarioAnterior[0].nombre}', 
         correo_electronico='${correo_electronico ? correo_electronico : usuarioAnterior[0].correo_electronico}', 
         tipo_usuario='${tipo_usuario ? tipo_usuario : usuarioAnterior[0].tipo_usuario}', 
-        estado='${estado ? estado : usuarioAnterior[0].estado}' where identificacion=? `, [identificacion])
+        estado=1 where identificacion=? `, [identificacion])
 
          if (resultado.affectedRows > 0) {
 
