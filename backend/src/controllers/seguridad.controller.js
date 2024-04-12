9c002b6fb5a68ff8182736a6e804f4089797ee13
Luis1080931 <luisu0616@gmail.com>
2024-04-12 16:43:37 -0500

    changes in controller security

--- backend/src/controllers/seguridad.controller.js
+++ backend/src/controllers/seguridad.controller.js
@@ -9,12 +9,12 @@ export const validar = async (req, res) => {
         let {correo_electronico, password} = req.body
         let sql = `SELECT identificacion, nombre, telefono, tipo_usuario, estado FROM usuarios WHERE correo_electronico='${correo_electronico}' and password='${password}'`
 
-        const [rows] = await pool.query(sql)
+        const [user] = await pool.query(sql)
 
-        if(rows.length>0){
-            let token = jwt.sign({rows}, process.env.AUT_SECRET, {expiresIn:process.env.AUT_EXPIRE})
+        if(user.length>0){
+            let token = jwt.sign({user}, process.env.AUT_SECRET, {expiresIn:process.env.AUT_EXPIRE})
 
-            return res.status(200).json({ 'user':rows,'token':token})
+            return res.status(200).json({ 'user':user,'token':token})
         }else{
             return res.status(404).json({'status': 404, 'message': 'Usuario no autorizado'})
         }
