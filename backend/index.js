b6d45766e1d900e961164114b7f14899c7256bb0
Luis1080931 <luisu0616@gmail.com>
2024-04-11 22:57:36 -0500

    index original

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,11 @@
 import express  from "express"
 import body_parser from "body-parser"
-import RouteMuestras from "./src/routes/routeMuestras.js";
-import routeVariables from "./src/routes/routeVariables.js";
-import cors from 'cors'
 
 const servidor = express()
-servidor.use(cors())
 
-servidor.use(body_parser.json());
-servidor.use(body_parser.urlencoded({ extend : false }));
+servidor.use(express.json());
+servidor.use(express.urlencoded({ extended : false }));
 //listen 
-servidor.use('/muestras', RouteMuestras)
-servidor.use('/variables', routeVariables)
 servidor.listen(3000, () => {
     console.log('Servidor rodando na porta 3000')
 })
