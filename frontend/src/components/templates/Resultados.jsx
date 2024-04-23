962b3e719b21a2550d03942752780ed487aee203
Luis1080931 <luisu0616@gmail.com>
2024-04-23 11:52:26 -0500

    add changes in frontend

--- frontend/src/components/templates/Resultados.jsx
+++ frontend/src/components/templates/Resultados.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FormResultados from '../molecules/FormResultados.jsx';
 import { ModalAcciones } from '../organisms/Modal.jsx';
+import { useResultado } from '../../context/ResultadosContext.jsx';
 
 
 function ResultadosModal ({ open, onClose, handleSubmit, actionLabel, title, mode, initialData}){
