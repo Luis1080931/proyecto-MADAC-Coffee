1366ee8266e364fda4e2ac0ef07f901d2eecd39b
Luis1080931 <luisu0616@gmail.com>
2024-04-23 22:19:17 -0500

    añadir validacion de resultados para registrar y actualizar

--- backend/src/routes/route.resultados.js
+++ backend/src/routes/route.resultados.js
@@ -6,8 +6,8 @@ import { validarToken } from "../controllers/seguridad.controller.js";
 const routeResultados = Router()
 
 routeResultados.get('/listar',validarToken, listarResultados)
-routeResultados.post('/registrar', validarToken, registrarResultados)
-routeResultados.put('/actualizar/:id', validarToken, actualizarResultado)
+routeResultados.post('/registrar', validarToken, validarResultados, registrarResultados)
+routeResultados.put('/actualizar/:id', validarToken, validarResultados, actualizarResultado)
 routeResultados.put('/desactivar/:idResultado',validarToken, desactivarResultado)
 routeResultados.get('/buscar/:idResultado',validarToken, buscarResultados)
 
