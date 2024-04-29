468b619dd7c3d1dc1dea8f20df7aaf103ff23d20
alejandroDpAL <alejandropasaje799@gmail.com>
2024-04-29 11:50:27 -0500

    registro variable

--- backend/src/routes/routeVariables.js
+++ backend/src/routes/routeVariables.js
@@ -7,7 +7,7 @@ const routeVariables = Router()
 
 routeVariables.get("/listarvariable", validarToken, listarVariables)
 
-routeVariables.post("/crearvariable", validarToken, validacionVariable, CrearVariable)
+routeVariables.post("/crearvariable", validarToken, validacionVariable,  CrearVariable)
 
 routeVariables.put("/actualizarvariable/:codigo",validarToken,validacionVariable, ActualizarVariable)
 routeVariables.put("/desactivarVariable/:codigo",validarToken, desactivarVariable)
