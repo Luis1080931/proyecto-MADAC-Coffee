3151046d48358bb7e9599e248a901b0cc7bed430
Luis1080931 <luisu0616@gmail.com>
2024-05-07 11:42:56 -0500

    agregando cambios en la grafica estadistica

--- frontend/src/components/organisms/Estadisticas.jsx
+++ frontend/src/components/organisms/Estadisticas.jsx
@@ -8,26 +8,30 @@ const AnalisisFisicosChart = () => {
   useEffect(() => {
     const fetchData = async () => {
       const result = await axiosClient.get('/analisis/listar');
-      setData(result.data);
+      setData(result.data)
     };
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
+  // Mapea los datos para x (mes) y y (cantidad de análisis)
   const chartData = data.map(item => ({ x: item.mes, y: item.cantidad }));
 
+  // Configuración de los nombres de los meses para el eje x
+  const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
   return (
-    <div>
-      <h2>Gráfico de cantidad de análisis físicos en los últimos 5 meses</h2>
+    <div className='w-7/12'>
+      <h2 className='text-2xl'>Gráfico de cantidad de análisis físicos en los últimos 5 meses</h2>
       <VictoryChart width={600} height={400} padding={{ left: 80, right: 50, top: 50, bottom: 50 }}>
         <VictoryLabel text="Mes" x={300} y={30} textAnchor="middle"/>
         <VictoryLabel text="Cantidad" x={30} y={200} angle={-90} textAnchor="middle"/>
-        <VictoryAxis tickFormat={t => t.substring(0, 3)} />
+        <VictoryAxis tickValues={months} tickFormat={(t) => months.indexOf(t) + 1} />
         <VictoryAxis dependentAxis />
         <VictoryLine data={chartData} />
       </VictoryChart>
     </div>
-  );
-};
+  )
+}
 
 export default AnalisisFisicosChart;
