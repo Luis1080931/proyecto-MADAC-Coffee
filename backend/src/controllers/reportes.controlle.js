ad8b8c7a3a6218260d86dd4bd066f1424272fdac
Luis1080931 <luisu0616@gmail.com>
2024-05-22 22:26:31 -0500

    modificando endpoint de reportes, generando un solo json

--- backend/src/controllers/reportes.controlle.js
+++ backend/src/controllers/reportes.controlle.js
@@ -19,9 +19,8 @@ export const generarPDF = async (req, res) => {
             m.fecha AS fecha,
             m.proceso_fermentacion,
             m.humedad_cafe,
-            m.tipo_secado, 
-            r.codigo AS resultado_id,
-            r.fk_variables,
+            m.tipo_secado,
+            var.nombre AS variable,
             r.valor
         FROM 
             analisis a
@@ -39,12 +38,15 @@ export const generarPDF = async (req, res) => {
             usuarios c ON f.fk_caficultor = c.identificacion
         JOIN 
             resultados r ON a.codigo = r.fk_analisis
+        JOIN 
+            variables var ON fk_variables = var.v_codigo
         WHERE 
             a.codigo = ?;
     `
     const [result] = await pool.query(sql, [id])
     if(result.length>0){
-        res.status(200).json(result)
+        const consolidatedResult = reestructurarDatos(result);
+        res.status(200).json(consolidatedResult);
     }else{
         res.status(404).json({
            'status': 404,
@@ -58,6 +60,39 @@ export const generarPDF = async (req, res) => {
         })
     }
 }
+
+function reestructurarDatos(datos) {
+    if (datos.length === 0) return null;
+
+    const resultadoFinal = {
+        analisis_id: datos[0].analisis_id,
+        caficultor_nombre: datos[0].caficultor_nombre,
+        finca_id: datos[0].finca_id,
+        municipio: datos[0].municipio,
+        vereda: datos[0].vereda,
+        nombre_finca: datos[0].nombre_finca,
+        lote_id: datos[0].lote_id,
+        variedad: datos[0].variedad,
+        muestra_id: datos[0].muestra_id,
+        altura_MSNM: datos[0].altura_MSNM,
+        fecha: datos[0].fecha,
+        proceso_fermentacion: datos[0].proceso_fermentacion,
+        humedad_cafe: datos[0].humedad_cafe,
+        tipo_secado: datos[0].tipo_secado,
+        resultados: []
+    };
+
+    datos.forEach(row => {
+        resultadoFinal.resultados.push({
+            resultado_id: row.resultado_id,
+            variable: row.variable,
+            valor: row.valor
+        });
+    });
+
+    return resultadoFinal;
+}
+
 
 export const listarDatos = async (req, res) => {
     try {
