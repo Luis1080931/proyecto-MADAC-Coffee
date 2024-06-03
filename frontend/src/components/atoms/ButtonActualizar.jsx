15c57c54351a1ad9736cf44ffdc7732e6122526e
Luis1080931 <luisu0616@gmail.com>
2024-06-02 22:56:22 -0500

    agregando modal para ver todos los datos de las muestras

--- frontend/src/components/atoms/ButtonActualizar.jsx
+++ frontend/src/components/atoms/ButtonActualizar.jsx
@@ -5,6 +5,6 @@ import { FiEdit3 } from "react-icons/fi";
 export const ButtonActualizar = (props) => {
   
   return (
-    <FiEdit3 className='cursor-pointer text-3xl text-gray-500 mr-10' onClick={props.click}/>
+    <FiEdit3 className='cursor-pointer text-3xl text-gray-500 mr-5' onClick={props.click}/>
   )
 }
