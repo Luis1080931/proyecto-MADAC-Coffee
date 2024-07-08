7b58e637a8d4829136f75c2b73abda29e1cf993d
MariaTorres7 <maria1033097029@gmail.com>
2024-07-08 08:40:14 -0500

    modificacion de la documentacion de analisis

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ servidor.set('views', './views')
 servidor.use(express.static('./public'))
 
 servidor.get('/document', (req, res) => {
-    res.render('documentAnalisis.ejs')
+    res.render('documentñ.ejs')
 })
 
 //listen 
